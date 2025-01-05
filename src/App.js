5068382c14e429b455a9da89475cea41a2cb6451
fffadedev <omarjohnson2004@gmail.com>
2025-01-05 18:10:13 -0500

    Fixed HashRouter

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ function App() {
         <HashRouter>
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/about/" element={<About />} />
-            <Route path="/contact/" element={<Contact />} />
+            <Route path="/#/about/" element={<About />} />
+            <Route path="/#/contact/" element={<Contact />} />
           </Routes>
         </HashRouter>
 
