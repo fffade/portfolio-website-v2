5ed97b98949e1ba1891ecfdce2130eb0844eb8e8
fffadedev <omarjohnson2004@gmail.com>
2025-01-05 18:47:26 -0500

    Changed HashRouter

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,9 @@ function Header() {
   return (
     <header className="container-fluid flex flex-row text-2xl text-neutral-400 font-medium w-screen min-h-52 justify-center">
       <div className="w-1/4 h-min flex flex-row justify-evenly self-end">
-        <Link to="/" className="transition-colors hover:text-neutral-500">Home</Link>
-        <Link to="/about" className="transition-colors hover:text-neutral-500">About</Link>
-        <Link to="/contact" className="transition-colors hover:text-neutral-500">Contact</Link>
+        <Link to="" className="transition-colors hover:text-neutral-500">Home</Link>
+        <Link to="about" className="transition-colors hover:text-neutral-500">About</Link>
+        <Link to="contact" className="transition-colors hover:text-neutral-500">Contact</Link>
       </div>
     </header>
   );
