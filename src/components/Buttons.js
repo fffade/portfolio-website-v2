9c8e5764f120da14d4cdbcc65cea3fe33bc13c2c
fffadedev <omarjohnson2004@gmail.com>
2025-01-14 14:29:44 -0500

    Completed base layout for Projects page

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -8,13 +8,13 @@
 
 function PrimaryButton(props) {
   return (
-     <button onClick={props.clickHandle || (() => {})} className="bg-neutral-300 block w-full h-16 m-4 text-2xl text-center transition-transform hover:scale-105"><i className={props.icon} /> {props.text}</button>
+     <button onClick={props.clickHandle || (() => {})} className="bg-neutral-300 block w-full h-16 mt-4 mb-4 text-2xl text-center transition-transform hover:scale-105"><i className={props.icon} /> {props.text}</button>
   );
 }
 
 function SubmitButton() {
   return (
-    <button type="submit" className="w-1/3 p-4 m-8 rounded-full self-center bg-slate-700 text-2xl text-white font-bold transition-all hover:scale-105 hover:text-gray-300">SUBMIT</button>
+    <button type="submit" className="w-1/3 p-4 mt-8 mb-8 rounded-full self-center bg-slate-700 text-2xl text-white font-bold transition-all hover:scale-105 hover:text-gray-300">SUBMIT</button>
   );
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,6 +12,33 @@ import { useNavigate } from 'react-router';
 import ReactDOM from 'react-dom/client';
 
 
+// component to showcase one project
+function Project(props)
+{
+  return (
+    <div className="grid grid-cols-2 grid-rows-2 w-full min-h-64 border-b-2 mt-8 p-2">
+
+      { /* A project component is split into three sections, the info, the image, and then the links/misc. */ }
+      <span className="flex flex-col gap-y-2">
+        <h1 className="text-4xl text-gray-100">{props.name}</h1>
+        <p className="text-2xl text-gray-300">{props.tagline}</p>
+        <p className="text-2xl text-gray-300">Team of {props.size || 1}</p>
+      </span>
+
+      <span className="row-span-2 flex flex-row justify-center items-center">
+        <img className="h-5/6 max-w-full text-white rounded-xl shadow-slate-800 shadow-lg" src={props.img} alt={`Project ${props.name}`} />
+      </span>
+
+      <span className="self-end">
+        <a className="text-xl text-neutral-400 underline" target="_blank" href={props.github}><i className="fa-brands fa-github" />View on GitHub</a>
+        <p className="text-xl text-neutral-500">{props.keywords.join(', ')}</p>
+      </span>
+
+    </div>
+  );
+}
+
+
 function Projects() {
 
   const navigate = useNavigate();
@@ -21,9 +48,20 @@ function Projects() {
   return (
     <div className="container-fluid flex flex-col overflow-x-hidden w-full min-h-screen bg-slate-900">
 
-        <main className="container-fluid self-center flex flex-col w-1/4 min-h-3/4 m-8 ">
+        <main className="container-fluid self-center flex flex-col w-1/3 min-h-3/4 m-8 ">
 
           <PrimaryButton clickHandle={() => { navigate('/'); }} icon="fa-solid fa-arrow-up" text="GO BACK" />
+
+          { /* List of all projects */ }
+          <div className="container-fluid flex flex-col w-full p-4">
+
+            <Project name="Moffat Bay Lodge" tagline="A resort website with booking and customer reservations."
+              size={2}
+              img="/assets/projects/bay_lodge.jpg"
+              keywords={['Java', 'JSP', 'Web Development', 'User Experience', 'SQL']}
+              github="https://github.com/fffade/MoffatBay"/>
+
+          </div>
 
         </main>
 
