8644ed5e44e256f62872a022fcbe72201d40be8a
fffadedev <omarjohnson2004@gmail.com>
2025-01-20 17:54:16 -0500

    Changed learn more button to view full portfolio document

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,7 +11,7 @@ import React from 'react';
 
 function HomePage() {
 
-  const navigate = useNavigate();
+  const PORTFOLIO_URL = "https://docs.google.com/document/d/19EbnN8Limoq_Ucs27Xi2iWOIxdepigd5cwDOoUBDqbc/edit?usp=sharing";
 
   return (
     <div className="container-fluid bg-slate-800 flex flex-col min-h-1/4 items-center justify-center text-center gap-y-12 p-20">
@@ -23,7 +23,7 @@ function HomePage() {
 
      <div className="buttons container-fluid flex flex-col w-1/2 md:w-1/3 lg:w-1/5 text-black items-center justify-between">
        <Buttons.PrimaryButton clickHandle={() => { window.location.href = "/assets/resume.pdf"; }} icon="fa-solid fa-download" text="GET MY RESUME" />
-       <Buttons.PrimaryButton clickHandle={() => { navigate('/about'); }} icon="fa-solid fa-arrow-right" text="LEARN MORE" />
+       <Buttons.PrimaryButton clickHandle={() => { window.location.href = PORTFOLIO_URL; }} icon="fa-solid fa-eye" text="VIEW MY WORK" />
      </div>
     </div>
   );
